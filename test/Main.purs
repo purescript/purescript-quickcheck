41bf3c45e5a4ebffd26f1cd23bf78a0cd1708ec9
Harry Garrood <harry@garrood.me>
2015-07-31 13:16:33 +0100

    Fix tests

--- test/Main.purs
+++ test/Main.purs
@@ -2,6 +2,9 @@
 module Test.Main where
 
 import Prelude
+import Control.Bind
+import Data.Array (head)
+import Data.Maybe.Unsafe (fromJust)
 import Data.Foldable
 import Test.QuickCheck.Gen
 import Test.QuickCheck.Arbitrary
@@ -9,15 +12,14 @@ import Control.Monad.Eff.Console
 
 main = do
   log "Try with some little Gens first"
-  print $ go 10
-  print $ go 100
-  print $ go 1000
-  print $ go 10000
+  print =<< go 10
+  print =<< go 100
+  print =<< go 1000
+  print =<< go 10000
 
   log "Testing stack safety of Gen"
-  print $ go 20000
+  print =<< go 20000
 
   where
-  go n = sum $ _.value $ runGen (getVector n) state
-  getVector n = vectorOf n (arbitrary :: Gen Int)
-  state = { newSeed: 0, size: 100 }
+  go n = map (sum <<< unsafeHead) $ randomSample' 1 (vectorOf n (arbitrary :: Gen Int))
+  unsafeHead = fromJust <<< head
