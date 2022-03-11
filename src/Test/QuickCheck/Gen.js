1346630386e09468ef03872cad3578c4f78045aa
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-03-11 14:20:40 -0800

    Migrated FFI to ES modules via 'lebab'

--- src/Test/QuickCheck/Gen.js
+++ src/Test/QuickCheck/Gen.js
@@ -3,10 +3,10 @@
 
 // module Test.QuickCheck.Gen
 
-exports.float32ToInt32 = function (n) {
+export function float32ToInt32(n) {
   var arr = new ArrayBuffer(4);
   var fv = new Float32Array(arr);
   var iv = new Int32Array(arr);
   fv[0] = n;
   return iv[0];
-};
+}
