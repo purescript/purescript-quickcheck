76b7beb988535663535faf4a288cc51dd3ea2269
JordanMartinez <jordanalex.martinez@protonmail.com>
2022-03-15 14:55:42 -0700

    Update to v0.15.0 (#130)
    * Migrated FFI to ES modules via 'lebab'
    
    * Removed '"use strict";' in FFI files
    
    * Update to CI to use 'unstable' purescript
    
    * Update pulp to 16.0.0-0 and psa to 0.8.2
    
    * Update Bower dependencies to master
    
    * Update .eslintrc.json to ES6
    
    * Added changelog entry

--- src/Test/QuickCheck/Gen.js
+++ src/Test/QuickCheck/Gen.js
@@ -1,12 +1,10 @@
 /* global ArrayBuffer, Float32Array, Int32Array */
-"use strict";
-
 // module Test.QuickCheck.Gen
 
-exports.float32ToInt32 = function (n) {
+export function float32ToInt32(n) {
   var arr = new ArrayBuffer(4);
   var fv = new Float32Array(arr);
   var iv = new Int32Array(arr);
   fv[0] = n;
   return iv[0];
-};
+}
